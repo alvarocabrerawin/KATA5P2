da2dc73201b8fff326a55a33d32e29ec51bbb41c
alvar <alvar@DESKTOP-52OFONO>
2021-11-04 21:32:35 +0000

    Kata4 Version2

--- src/kata4/main/main.java
+++ src/kata4/main/main.java
@@ -5,20 +5,45 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import kata4.model.Histogram;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import kata4.view.HistogramDisplay;
 import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 import kata4.model.Mail;
+
 public class main {
     public static void main(String args[]) throws IOException{
-        String filename = "C:\\Users\\alvar\\OneDrive\\Documentos\\NetBeansProjects\\Kata4\\src\\kata4\\main\\email.txt";
+        
+        main mainc = new main();
+        mainc.control();
+        
+    }
+    
+    private void control() throws IOException{
+        input();
+        process();
+        output();
+    }
+    
+    private String filename;
+    private Histogram<String> histogram;
+    private void input() {
+        filename = "C:\\Users\\alvar\\OneDrive\\Documentos\\NetBeansProjects\\Kata4\\src\\kata4\\main\\email.txt";
+    }
+
+    
+    private void process() throws IOException {
         List<Mail> mailList = MailListReader.read(filename);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    
+    private void output() {
         HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
-
-}
     
+}   
     
 
